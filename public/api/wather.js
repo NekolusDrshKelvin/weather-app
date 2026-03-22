417a139fbf6c321f274095e90b1193267fc461c3
Kelvin Drsith Nekolus <ksett5953@gmail.com>
2026-03-22 09:45:51 +0630

    Add weather API handler in wather.js

---
+++ public/api/wather.js
@@ -0,0 +1,16 @@
+export default async function handler(req, res) {
+  const { city } = req.query;
+
+  const API_KEY = process.env.OPENWEATHER_API_KEY;
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
+
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+
+    res.status(200).json(data);
+  } catch (err) {
+    res.status(500).json({ error: "Server error" });
+  }
+}
